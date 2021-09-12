93c2ec6d78af0094a2ccefcf5d655888d804b029
joyboy <ram.einstein42@gmail.com>
2021-09-11 23:09:46 -0700

    pygal

---
+++ die_visual.py
@@ -0,0 +1,30 @@
+from die import Die
+import pygal
+
+die = Die()
+
+# roll and store in list
+results = []
+for roll_num in range(1000):
+    result = die.roll()
+    results.append(result)
+
+# analyze results
+frequencies = []
+for value in range(1, die.num_sides + 1):
+    frequency = results.count(value)
+    frequencies.append(frequency)
+print(frequencies)
+
+# visual results
+
+
+hist = pygal.Bar()
+
+hist.title = "Result of rolling die 1000 times"
+hist.x_labels = ['1', '2', '3', '4', '5', '6']
+hist.x_title = "Result"
+hist.y_title = "Frequency of Result"
+
+hist.add('D6',frequencies)
+hist.render_to_file('die_visual.svg')
